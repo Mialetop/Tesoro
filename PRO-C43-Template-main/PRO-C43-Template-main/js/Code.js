fa8d0c24bf246e07179fe9472efd8f4add406b53
Mialetop <93691086+Mialetop@users.noreply.github.com>
2022-02-12 22:37:56 -0600

    Add files via upload

---
+++ PRO-C43-Template-main/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("E D A T E D O S B A S", 700,180)
+    fill("lightblue")
+    text("C U N I F O N", 100,285);
+// add code for displaying rest of the names and the hints.
+    
+
+}
